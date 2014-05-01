3d3ccc1ea684a4c1900754362a0a5fb489bbaced
askhalsa <akhalsa@live.com>
2014-05-01 18:12:51 -0400

    Update server.js

--- mobile/server.js
+++ mobile/server.js
@@ -1,3 +1,9 @@
+//Amritpal Khalsa, CSE '15 
+//297DP Spring 2014 
+//askhalsa@umass.edu 
+//The server file running on a Heroku server
+
+
 var port = process.env.PORT || 3000;
  
 var app = require('express').createServer();
@@ -18,22 +24,22 @@ io.configure(function(){
 		"htmlfile"]);
 	io.set("polling duration", 1000);
 });
- 
-io.sockets.on("connection", function(socket){
+//Heroku server acts as a middle man, html website talks to Heroku server, Heroku server talks to beaglebone and vice versa
+io.sockets.on("connection", function(socket){//this function defines what will happen when the Heroku server receives a signal
 	socket.on("beaglebone", function(data){
 	    if(data == "test"){
 		io.sockets.emit("beaglebone", {"message": "Server is running!!"});
 	    }
 	});
-	socket.on("led", function(data){
-            if(data == "on"){
-		io.sockets.emit("beaglebone", "on");
+	socket.on("led", function(data){//when the user clicks "open" or "close", the Heroku server 
+            if(data == "on"){           //signals the beaglebone
+		io.sockets.emit("beaglebone", "on");//emit is used to send out data for a certain event (beaglebone)
 		}
 	    else if(data == "off"){
 		io.sockets.emit("beaglebone", "off");
 		}            
-            else {
-               io.sockets.emit("beaglebone", data);
+            else {                                 //and if the user does anything besides "open" or "close", such as "set"
+               io.sockets.emit("beaglebone", data);//the Heroku server will send data to the beaglebone
             }
 	});
 });
